181f726900829be11cf7003bd0de0f8c416ab2d5
lbenelisha-cpu <lbenelisha@gmail.com>
2026-09-10 11:04:22 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,1 @@
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://anzpffgcyrkumpojiqic.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_dC0Qx7c9gSiK1chE1fFwhA_oAUKL8TX"
-};
+window.APP_CONFIG = { marketEndpoint: "/.netlify/functions/market-data" };
